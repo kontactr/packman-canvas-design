c7f963f0c861ef4484eeeafe31a38a4275d774f9
Yash Gandhi <yash.g@simformsolutions.com>
2019-01-25 11:15:20 +0530

    beans added

--- js/beans.js
+++ js/beans.js
@@ -1,107 +1,241 @@
 let beansObject = [];
 
-let steps = [
-    [30,26],
-    [30,50],
-    [195,50],
-    [610,50]
-];
+let steps = [[30, 26], [30, 50], [195, 50], [610, 50]];
 
-
-//first row 
-for(let index=30 ; index < 760 ; index += 25 ){
-    beansObject.push({
-        x:index,
-        y:26,
-        eat: false
-    });
-
+//first row
+for (let index = 30; index < 760; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 26,
+    eat: false
+  });
 }
 
 //second row - first section
-for(let index=30;index<100;index+=25){
-    beansObject.push({
-        x:index,
-        y:50,
-        eat: false
-    });
+for (let index = 30; index < 100; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 50,
+    eat: false
+  });
 }
 
 //second row - second section
-for(let index=195 ; index < 510 ; index+=25){
-    beansObject.push({
-        x:index,
-        y:50,
-        eat: false
-    });
+for (let index = 195; index < 510; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 50,
+    eat: false
+  });
 }
 
 //second row - third section
-for(let index=610 ; index < 760 ; index+=25){
-        beansObject.push({
-            x:index,
-            y:50,
-            eat: false
-        });
+for (let index = 610; index < 760; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 50,
+    eat: false
+  });
 }
 
 //Third row for 30
-for(let index=30;index<100;index+=25){
-    beansObject.push({
-        x:index,
-        y:74,
-        eat: false
-    });
+for (let index = 30; index < 100; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 74,
+    eat: false
+  });
+}
+
+//third row - second section
+for (let index = 195; index < 220; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 74,
+    eat: false
+  });
+}
+
+//third row - third section
+for (let index = 345; index < 350; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 74,
+    eat: false
+  });
+}
+
+//third row - fourth section
+for (let index = 495; index < 500; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 74,
+    eat: false
+  });
+}
+
+//third row - fourth section
+for (let index = 610; index <= 620; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 74,
+    eat: false
+  });
 }
 
 //fourth row
-for(let index=30;index<100;index+=25){
-    beansObject.push({
-        x:index,
-        y:98,
-        eat: false
-    });
+for (let index = 30; index < 100; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 98,
+    eat: false
+  });
+}
+
+//fourth row - second section
+for (let index = 195; index < 220; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 98,
+    eat: false
+  });
+}
+
+//fourth row - third section
+for (let index = 345; index < 350; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 98,
+    eat: false
+  });
+}
+
+//fourth row - fourth section
+for (let index = 495; index < 500; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 98,
+    eat: false
+  });
+}
+
+//fourth row - fifth section
+for (let index = 610; index <= 620; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 98,
+    eat: false
+  });
 }
 
 //fifth row
-for(let index=30;index<100;index+=25){
-    beansObject.push({
-        x:index,
-        y:146,
-        eat: false
-    });
+for (let index = 30; index < 100; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 146,
+    eat: false
+  });
+}
+
+//fifth row - second section
+for (let index = 195; index < 220; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 146,
+    eat: false
+  });
+}
+
+//fifth row - third section
+for (let index = 345; index < 350; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 146,
+    eat: false
+  });
+}
+
+//fifth row - fourth section
+for (let index = 495; index < 500; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 146,
+    eat: false
+  });
+}
+
+//fifth row - six section
+for (let index = 685; index < 700; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 146,
+    eat: false
+  });
 }
 
 //six row
-for(let index=30;index<100;index+=25){
-    beansObject.push({
-        x:index,
-        y:170,
-        eat: false
-    });
+for (let index = 30; index < 100; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 170,
+    eat: false
+  });
+}
+
+//six row - second section
+for (let index = 195; index < 220; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 170,
+    eat: false
+  });
+}
+
+//six row - third section
+for (let index = 345; index < 350; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 170,
+    eat: false
+  });
+}
+
+//six row - fourth section
+for (let index = 495; index < 500; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 170,
+    eat: false
+  });
+}
+
+//six row - six section
+for (let index = 685; index < 700; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 170,
+    eat: false
+  });
 }
 
 //seven row
-for(let index=30;index<760;index+=25){
-    beansObject.push({
-        x:index,
-        y:194,
-        eat: false
-    });
+for (let index = 30; index < 760; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 194,
+    eat: false
+  });
 }
 
 //eight row
-for(let index=30;index<760;index+=25){
-    beansObject.push({
-        x:index,
-        y:218,
-        eat: false
-    });
+for (let index = 30; index < 760; index += 25) {
+  beansObject.push({
+    x: index,
+    y: 218,
+    eat: false
+  });
 }
 
-
-
-
-function getBeans(){
-    return beansObject;
+function getBeans() {
+  return beansObject;
 }
--- js/index.js
+++ js/index.js
@@ -3,52 +3,41 @@ let y = 15;
 let image = new Image();
 let q = null;
 
-document.addEventListener("DOMContentLoaded" , (e) => {
+document.addEventListener("DOMContentLoaded", e => {
+  image.src = "js/pacman.png";
+  image.onload = e => {
+    console.log(image, "Aaa");
+    q = new Packman();
+    console.log(q);
+  };
 
-    
-    image.src = "js/pacman.png";
-    image.onload = (e) => {
-        console.log(image , "Aaa"  );
-        q = new Packman();
-        console.log(q);
-    };
-
-    animate();
-
-    
-
+  animate();
 });
 
+function animate() {
+  let canvas = document.getElementById("canvas");
+  let context = canvas.getContext("2d");
+  //context.clearRect(0,0,canvas.width , canvas.height);
 
-function animate(){
-
-    
-    
-    let canvas = document.getElementById("canvas");
-    let context = canvas.getContext("2d");
-    //context.clearRect(0,0,canvas.width , canvas.height);
-    
-
-    /*if(x <= canvas.width - 20){
+  /*if(x <= canvas.width - 20){
         x+= 1;
     }else{
         y+= 25;
         x = 15;
     }*/
-    //console.log(x,y);
-    
-    //console.log(getImageData());
-    setBackground(context , canvas.width , canvas.height);
-    setBoundries(context);
-    innerObstracles(context , canvas.width , canvas.height);
-    
-    if(q)
-    drawPackMan(context,q.x,q.y);
-    beans(context);
-    requestAnimationFrame(animate);
-    context.fillStyle = "white";
-    //context.translate()
-    /*if(q){
+  //console.log(x,y);
+
+  //console.log(getImageData());
+  setBackground(context, canvas.width, canvas.height);
+  setBoundries(context);
+  innerObstracles(context, canvas.width, canvas.height);
+
+  if (q) drawPackMan(context, q.x, q.y);
+  beans(context);
+  requestAnimationFrame(animate);
+  context.fillStyle = "white";
+  //context.translate()
+  /*if(q){
     console.log(q.x , q.y);
     context.translate(q.x,q.y);
     context.rotate(90 * Math.PI / 180);
@@ -58,168 +47,155 @@ function animate(){
 }*/
 }
 
-function drawPackMan(context,x,y){
-    //context.rotate(90 * Math.PI / 180);
-    
-    context.translate(72,26);
-    context.rotate(90 * Math.PI / 180);
-    context.drawImage(image, 0,0,27,27);
-    context.rotate(- (90 * Math.PI / 180));
-    context.translate(-72,-26);
-    //console.log(x);
-    
-    //context.rotate(-90 * Math.PI / 180);
+function drawPackMan(context, x, y) {
+  //context.rotate(90 * Math.PI / 180);
 
+  context.translate(72, 26);
+  context.rotate((90 * Math.PI) / 180);
+  context.drawImage(image, 0, 0, 27, 27);
+  context.rotate(-((90 * Math.PI) / 180));
+  context.translate(-72, -26);
+  //console.log(x);
+
+  //context.rotate(-90 * Math.PI / 180);
 }
 
+function beans(context) {
+  let beanData = getBeans();
 
-function beans(context){
+  for (let k = 0; k < beanData.length; k++) {
+    context.fillRect(beanData[k].x, beanData[k].y, 5, 5);
+  }
 
-            let beanData = getBeans();
+  //for(let index=10 ; index<=x; index++){
+  //    for(let index1 = 10 ; index1<=y ; index1++)
+  //context.fillRect(30,26,5,5);
 
-            for(let k=0; k < beanData.length ; k++){
-                context.fillRect(beanData[k].x,beanData[k].y,5,5);
-            }
-
-
-            //for(let index=10 ; index<=x; index++){
-            //    for(let index1 = 10 ; index1<=y ; index1++)
-                    //context.fillRect(30,26,5,5);
-                    
-            //}    
-            
-    
-    
-
+  //}
 }
 
-function innerObstracles(context , width , height){
+function innerObstracles(context, width, height) {
+  context.beginPath();
 
-    context.beginPath();
+  context.fillStyle = "blue";
+  context.strokeStyle = "green";
+  context.moveTo(110, 50);
 
-    context.fillStyle = "blue";
-    context.strokeStyle = "green";
-    context.moveTo(110,50);
+  //context.fillRect(110,50,5,5);
 
-    //context.fillRect(110,50,5,5);
+  context.strokeRect(110, 50, 70, 130);
 
-    context.strokeRect(110,50,70,130);
+  context.strokeStyle = "red";
+  context.moveTo(250, 93);
+  //context.fillRect(225,65,5,5);
+  context.strokeRect(225, 65, 100, 100);
 
-    context.strokeStyle = "red";
-    context.moveTo(250,90);
-    //context.fillRect(225,65,5,5);
-    context.strokeRect(225,65,100,100);
+  context.strokeStyle = "yellow";
+  context.strokeRect(250, 90, 50, 50);
 
-    context.strokeStyle = "yellow";
-    context.strokeRect(250,90,50,50);
+  context.moveTo(370, 90);
+  //context.fillRect(370,65,5,5);
+  context.strokeRect(370, 65, 100, 100);
 
-    context.moveTo(370,90);
-    //context.fillRect(370,65,5,5);
-    context.strokeRect(370,65,100,100);
+  context.strokeStyle = "red";
+  context.strokeRect(395, 90, 50, 50);
 
-    context.strokeStyle = "red";
-    context.strokeRect(395,90,50,50);
+  context.moveTo(520, 50);
 
-    context.moveTo(520,50);
+  //context.fillRect(520,50,5,5);
+  context.strokeStyle = "aqua";
+  context.strokeRect(520, 50, 70, 130);
 
-    //context.fillRect(520,50,5,5);
-    context.strokeStyle = "aqua"
-    context.strokeRect(520,50,70,130);
+  context.moveTo(630, 60);
+  //context.fillRect(630,60,5,5);
+  context.strokeStyle = "brown";
+  context.strokeRect(640, 70, 25, 10);
 
-    context.moveTo(630,60);
-    //context.fillRect(630,60,5,5);
-    context.strokeStyle = "brown"
-    context.strokeRect(630,70,25,10);
+  context.moveTo(620, 115);
+  context.strokeStyle = "aqua";
+  //context.fillRect(620,115,5,5);
 
-    context.moveTo(620,115);
-    context.strokeStyle = "aqua";
-    //context.fillRect(620,115,5,5);
+  context.strokeStyle = "blue";
+  context.strokeRect(630, 115, 45, 10);
 
-    context.strokeStyle = "blue";
-    context.strokeRect(620,115,45,10);
+  context.moveTo(630, 170);
 
-    context.moveTo(630,170);
+  //context.fillRect(630,170,5,5);
 
+  context.strokeStyle = "aqua";
+  context.strokeRect(640, 160, 25, 10);
 
-    //context.fillRect(630,170,5,5);
-    
-    context.strokeStyle = "aqua";
-    context.strokeRect(630,160,25,10);
-
-    context.strokeStyle = "black";
-    context.closePath();
+  context.strokeStyle = "black";
+  context.closePath();
 }
 
-
-function setBackground(context , width , height){
-    context.fillStyle = "black";
-    context.fillRect(0,0,width , height);
+function setBackground(context, width, height) {
+  context.fillStyle = "black";
+  context.fillRect(0, 0, width, height);
 }
 
-function setBoundries(context){
+function setBoundries(context) {
+  context.beginPath();
+  context.moveTo(10, 10);
+  context.fillStyle = "blue";
+  context.strokeStyle = "blue";
 
-    context.beginPath();
-    context.moveTo(10,10);
-    context.fillStyle = "blue";
-    context.strokeStyle = "blue";
+  let newWidth = canvas.width - 20;
+  context.lineTo(canvas.width - 20, 10);
 
-    let newWidth = canvas.width - 20;
-    context.lineTo(canvas.width - 20 , 10);
+  let newHight = (canvas.height * 28) / 100;
+  context.lineTo(newWidth, newHight);
 
-    let newHight = canvas.height * 28 / 100;
-    context.lineTo(newWidth , newHight);
+  newWidth = canvas.width - (canvas.width * 11) / 100;
+  context.lineTo(newWidth, newHight);
 
-    newWidth = canvas.width - (canvas.width * 11 / 100);
-    context.lineTo(newWidth , newHight);
+  newHight = newHight + (canvas.height * 10) / 100;
+  context.lineTo(newWidth, newHight);
 
-    newHight = newHight + canvas.height * 10 / 100;
-    context.lineTo(newWidth , newHight);
+  newWidth = canvas.width - 20;
+  context.lineTo(newWidth, newHight);
 
-    newWidth = canvas.width - 20;
-    context.lineTo(newWidth , newHight);
+  newHight = newHight + (canvas.height * 20) / 100;
+  context.lineTo(newWidth, newHight);
 
-    newHight = newHight + canvas.height * 20 / 100;
-    context.lineTo(newWidth , newHight);
+  newWidth = canvas.width - (canvas.width * 11) / 100;
+  context.lineTo(newWidth, newHight);
 
-    newWidth = canvas.width - (canvas.width * 11 / 100);
-    context.lineTo(newWidth , newHight);
+  newHight = newHight + (canvas.height * 10) / 100;
+  context.lineTo(newWidth, newHight);
 
-    newHight = newHight + canvas.height * 10 / 100;
-    context.lineTo(newWidth , newHight);
+  newWidth = canvas.width - 20;
+  context.lineTo(newWidth, newHight);
 
-    newWidth = canvas.width - 20;
-    context.lineTo(newWidth , newHight);
+  newHight = canvas.height - 20;
+  context.lineTo(newWidth, newHight);
 
-    newHight = canvas.height - 20;
-    context.lineTo(newWidth , newHight);
+  newWidth = 10;
+  context.lineTo(newWidth, newHight);
 
-    newWidth = 10;
-    context.lineTo(newWidth , newHight);
+  newHight = canvas.height - (canvas.height * 45) / 100;
+  context.lineTo(newWidth, newHight);
 
-    newHight = canvas.height - canvas.height * 45 / 100;
-    context.lineTo(newWidth , newHight);
+  newWidth = 60;
+  context.lineTo(newWidth, newHight);
 
-    newWidth = 60;
-    context.lineTo(newWidth  , newHight);
+  context.lineTo(newWidth, newHight - 25);
 
-    context.lineTo(newWidth , newHight - 25);
+  newHight = newHight - 25;
+  context.moveTo(newWidth, newHight);
 
-    newHight = newHight - 25;
-    context.moveTo(newWidth  , newHight);
+  newWidth = 10;
+  context.lineTo(10, newHight);
 
-    newWidth = 10;
-    context.lineTo(10 , newHight);
+  context.lineTo(10, 10);
 
-    context.lineTo(10,10);
+  context.stroke();
 
-    context.stroke();
-    
-    context.fillRect(10,10,5,5);
-    //context.fillRect(canvas.width - 20,10,5,5);
-    context.fillRect(10 , canvas.height - 20 , 5, 5);
-    context.fillRect(canvas.width - 20 , canvas.height - 20 , 5 , 5);
+  context.fillRect(10, 10, 5, 5);
+  //context.fillRect(canvas.width - 20,10,5,5);
+  context.fillRect(10, canvas.height - 20, 5, 5);
+  context.fillRect(canvas.width - 20, canvas.height - 20, 5, 5);
 
-    context.strokeStyle = "black";
-    context.closePath();
-
+  context.strokeStyle = "black";
+  context.closePath();
 }
